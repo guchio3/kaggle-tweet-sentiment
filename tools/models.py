385b597e65e8c4917539a4910d81697aefa44781
guchio3 <astt.hwhw@gmail.com>
2020-05-20 01:40:54 +0900

    temp

--- tools/models.py
+++ tools/models.py
@@ -169,8 +169,8 @@ class RobertaModelWDualMultiClassClassifierHead(nn.Module):
             self.model.pooler.dense.out_features, 1, 1)
         self.classifier_conv_tail = nn.Conv1d(
             self.model.pooler.dense.out_features, 1, 1)
-        self.add_module('conv_output_head', self.classifier_conv_head)
-        self.add_module('conv_output_tail', self.classifier_conv_tail)
+        # self.add_module('conv_output_head', self.classifier_conv_head)
+        # self.add_module('conv_output_tail', self.classifier_conv_tail)
 
         # self.classifier_head = nn.Linear(
         #     self.model.pooler.dense.out_features, num_labels)
@@ -238,6 +238,40 @@ class Conv1dSame(nn.Module):
             return self.conv(x)
 
 
+class RobertaModelWDualMultiClassClassifierAndSegmentationHead(
+        RobertaModelWDualMultiClassClassifierHead):
+    def __init__(self, num_labels, pretrained_model_name_or_path):
+        super().__init__(num_labels, pretrained_model_name_or_path)
+        self.classifier_conv_segmentation = nn.Conv1d(
+            self.model.pooler.dense.out_features, 1, 1)
+
+    def forward(self, input_ids=None, attention_mask=None,
+                token_type_ids=None, position_ids=None, head_mask=None,
+                inputs_embeds=None, encoder_hidden_states=None,
+                encoder_attention_mask=None):
+        outputs = self.model(input_ids,
+                             attention_mask=attention_mask,
+                             token_type_ids=token_type_ids,
+                             position_ids=position_ids,
+                             head_mask=head_mask,
+                             inputs_embeds=inputs_embeds,
+                             encoder_hidden_states=encoder_hidden_states,
+                             encoder_attention_mask=encoder_attention_mask)
+        output = outputs[0]
+        output = torch.transpose(output, 1, 2)
+        output = self.dropout(output)
+        logits_head = self.classifier_conv_head(output).squeeze()
+        logits_tail = self.classifier_conv_tail(output).squeeze()
+        logits_segmentation = self.classifier_conv_segmentation(
+            output).squeeze()
+
+        # add hidden states and attention if they are here
+        outputs = ((logits_head, logits_tail,
+                    logits_segmentation),) + outputs[2:]
+
+        return outputs  # logits, (hidden_states), (attentions)
+
+
 class RobertaModelWDualMultiClassClassifierHeadV2(nn.Module):
     def __init__(self, num_labels, pretrained_model_name_or_path):
         super().__init__()
@@ -303,70 +337,6 @@ class RobertaModelWDualMultiClassClassifierHeadV2(nn.Module):
 
         # add hidden states and attention if they are here
         outputs = ((logits_head, logits_tail),) + outputs[2:]
-
-        return outputs  # logits, (hidden_states), (attentions)
-
-    def resize_token_embeddings(self, token_num):
-        self.model.resize_token_embeddings(token_num)
-
-
-class RobertaModelWDualMultiClassClassifierAndSegmentationHead(nn.Module):
-    def __init__(self, num_labels, pretrained_model_name_or_path):
-        super().__init__()
-        if pretrained_model_name_or_path:
-            if isinstance(pretrained_model_name_or_path, str):
-                self.model = RobertaModel.from_pretrained(
-                    pretrained_model_name_or_path)
-            else:
-                # for sub
-                self.model = RobertaModel(pretrained_model_name_or_path)
-        else:
-            raise NotImplementedError
-        self.num_labels = num_labels
-        self.dropout = nn.Dropout(0.2)
-        self.classifier_conv_head = nn.Conv1d(
-            self.model.pooler.dense.out_features, 1, 1)
-        self.classifier_conv_tail = nn.Conv1d(
-            self.model.pooler.dense.out_features, 1, 1)
-        self.classifier_conv_segmentation = nn.Conv1d(
-            self.model.pooler.dense.out_features, 1, 1)
-        self.add_module('conv_output_head', self.classifier_conv_head)
-        self.add_module('conv_output_tail', self.classifier_conv_tail)
-        self.add_module(
-            'conv_output_segmentation',
-            self.classifier_conv_segmentation)
-
-        # self.classifier_head = nn.Linear(
-        #     self.model.pooler.dense.out_features, num_labels)
-        # self.classifier_tail = nn.Linear(
-        #     self.model.pooler.dense.out_features, num_labels)
-
-        # self.add_module('fc_output_head', self.classifier_head)
-        # self.add_module('fc_output_tail', self.classifier_tail)
-
-    def forward(self, input_ids=None, attention_mask=None,
-                token_type_ids=None, position_ids=None, head_mask=None,
-                inputs_embeds=None, encoder_hidden_states=None,
-                encoder_attention_mask=None):
-        outputs = self.model(input_ids,
-                             attention_mask=attention_mask,
-                             token_type_ids=token_type_ids,
-                             position_ids=position_ids,
-                             head_mask=head_mask,
-                             inputs_embeds=inputs_embeds,
-                             encoder_hidden_states=encoder_hidden_states,
-                             encoder_attention_mask=encoder_attention_mask)
-        output = outputs[0]
-        output = torch.transpose(output, 1, 2)
-        output = self.dropout(output)
-        logits_head = self.classifier_conv_head(output).squeeze()
-        logits_tail = self.classifier_conv_tail(output).squeeze()
-        logits_segmentation = self.classifier_conv_segmentation(
-            output).squeeze()
-
-        # add hidden states and attention if they are here
-        outputs = ((logits_head, logits_tail,
-                    logits_segmentation),) + outputs[2:]
 
         return outputs  # logits, (hidden_states), (attentions)
 
